5d255550caa6932b101121c29f1a16dbcf324116
Minsung Huh <guitar9224@gmail.com>
2020-04-23 23:16:37 +0900

    위장

--- src/kr/co/chrisdev/hash/Camouflage.java
+++ src/kr/co/chrisdev/hash/Camouflage.java
@@ -1,5 +1,8 @@
 package kr.co.chrisdev.hash;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 제 설명
  * 스파이들은 매일 다른 옷을 조합하여 입어 자신을 위장합니다.
@@ -29,10 +32,32 @@ package kr.co.chrisdev.hash;
 
 public class Camouflage {
     public static void main(String[] args) {
+        String[][] cloths = {
+                {"yellow_hat", "headgear"},
+                {"blue_sunglasses", "eyewear"},
+                {"green_turban", "headgear"},
+        };
 
+        int count = camouflage(cloths);
+        System.out.println("count : " + count);
     }
 
     public static int camouflage(String[][] cloths) {
+        int answer = 1;
 
+        Map<String, Integer> clothesMap = new HashMap<>(); // 종류 : 갯수
+
+        for (int i = 0; i < cloths.length; i++) {
+            // 종류 여부 판단. 같은 종류 일 경우 Value + 1
+            clothesMap.put(cloths[i][1], clothesMap.getOrDefault(cloths[i][1], 0)+1);
+        }
+
+        // 경우의 수 체크 answer *= (옷 가지수 + 안 입을 경우)
+        for (int val : clothesMap.values()){
+            answer *= (val+1);
+        }
+
+        // 모두 다 안입는 경우는 존재하지 않으므로 -1
+        return answer-1;
     }
 }
